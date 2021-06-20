aa1f06d39c33b8f07c15432853afc0ab81c30097
ddxdydz_console <Lizun57@inbox.ru>
2021-06-20 21:16:33 +0700

    Make it better

--- play_OOP_way.py
+++ play_OOP_way.py
@@ -3,15 +3,6 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import List, Optional
 
-start_position_ship = {
-    0: (0, 3, 3),
-    1: (0, 0, 5),
-    2: (3, 0, 3),
-    3: (3, 3, 0),
-    4: (5, 0, 0)
-}
-is_on_start_position = False
-check_reverse = False
 messages = []
 
 
@@ -499,52 +490,9 @@ def make_draft(data: dict) -> DraftChoice:
 
 
 def make_turn(data: dict) -> BattleOutput:
-    global start_position_ship, check_reverse, is_on_start_position
     battle_state = BattleState.from_json(data)
     battle_output = BattleOutput()
     battle_output.UserCommands = []
-
-    # Realization of movement to a given starting position:
-    if not is_on_start_position:
-        # Check Reverse
-        if not check_reverse:
-            if battle_state.My[0].Position.get_cords()[0] > 15:
-                start_position_ship = \
-                    {key: tuple(30 - value[n] - 2 for n in range(len(value)))
-                     for key, value in start_position_ship.items()}
-            check_reverse = True
-        # Checking occupied positions
-        if all(map(lambda n_ship:
-                   battle_state.My[n_ship].Position.get_cords() == start_position_ship[n_ship],
-                   range(len(battle_state.My)))):
-            is_on_start_position = True
-        else:
-            # Move detection
-            for num_ship, cur_my_ship in enumerate(battle_state.My):
-                do_move = True
-                cur_my_ship.add_correct_move(Vector(*start_position_ship[num_ship]))
-                # Check the allies distance to prevent a collision
-                cur_friendly_ship_cords = \
-                    battle_state.get_all_blocks_pos(cur_my_ship.Move_vector.get_cords())
-                for friendly_ship in battle_state.My:
-                    if friendly_ship.Id != cur_my_ship.Id:
-                        sum_a_cords = battle_state.Ships_collision_pos[friendly_ship.Id] + \
-                                      cur_friendly_ship_cords
-                        if len(set(sum_a_cords)) != len(sum_a_cords):
-                            do_move = False
-                            break
-                if do_move:
-                    # Updating the battle_state.Ships_collision_pos
-                    battle_state.Ships_collision_pos[cur_my_ship.Id] = \
-                        battle_state.get_all_blocks_pos(
-                            cur_my_ship.Move_vector.get_cords())
-
-                    battle_output.UserCommands.append(
-                        UserCommand(Command="MOVE", Parameters=MoveCommandParameters(
-                            cur_my_ship.Id, cur_my_ship.Move_vector)))
-            battle_output.Message = f"To starting position..."
-            messages.clear()
-            return battle_output
 
     # Forming move commands cycle:
     for cur_my_ship in battle_state.My:  # we could sort it
@@ -694,7 +642,9 @@ def make_turn(data: dict) -> BattleOutput:
                     f_ship = battle_state.Ships_id_dict[f_ship_id]
                     e_ship = battle_state.Ships_id_dict[e_ship_id]
                     f_ship.Attack_vector = e_ship.Position
-                    op_ships_focused[e_ship_id].remove(f_ship_id)
+                    for e_ship_id_2, _ in sorted_op_ships_focused:
+                        if f_ship_id in op_ships_focused[e_ship_id_2]:
+                            op_ships_focused[e_ship_id_2].remove(f_ship_id)
                 break
 
     # TODO TESTING
@@ -727,7 +677,7 @@ def make_turn(data: dict) -> BattleOutput:
     return battle_output
 
 
-def local_test_play_game():
+def local_test_game():
     while True:
         raw_line = 'BattleState_for_test.json'
         with open(raw_line, 'r', encoding='utf8') as json_file:
---
+++ script_tests/on_start_position_script.py
@@ -0,0 +1,52 @@
+start_position_ship = {
+    0: (0, 3, 3),
+    1: (0, 0, 5),
+    2: (3, 0, 3),
+    3: (3, 3, 0),
+    4: (5, 0, 0)
+}
+is_on_start_position = False
+check_reverse = False
+
+
+# Realization of movement to a given starting position:
+if not is_on_start_position:
+    # Check Reverse
+    if not check_reverse:
+        if battle_state.My[0].Position.get_cords()[0] > 15:
+            start_position_ship = \
+                {key: tuple(30 - value[n] - 2 for n in range(len(value)))
+                 for key, value in start_position_ship.items()}
+        check_reverse = True
+    # Checking occupied positions
+    if all(map(lambda n_ship:
+               battle_state.My[n_ship].Position.get_cords() == start_position_ship[n_ship],
+               range(len(battle_state.My)))):
+        is_on_start_position = True
+    else:
+        # Move detection
+        for num_ship, cur_my_ship in enumerate(battle_state.My):
+            do_move = True
+            cur_my_ship.add_correct_move(Vector(*start_position_ship[num_ship]))
+            # Check the allies distance to prevent a collision
+            cur_friendly_ship_cords = \
+                battle_state.get_all_blocks_pos(cur_my_ship.Move_vector.get_cords())
+            for friendly_ship in battle_state.My:
+                if friendly_ship.Id != cur_my_ship.Id:
+                    sum_a_cords = battle_state.Ships_collision_pos[friendly_ship.Id] + \
+                                  cur_friendly_ship_cords
+                    if len(set(sum_a_cords)) != len(sum_a_cords):
+                        do_move = False
+                        break
+            if do_move:
+                # Updating the battle_state.Ships_collision_pos
+                battle_state.Ships_collision_pos[cur_my_ship.Id] = \
+                    battle_state.get_all_blocks_pos(
+                        cur_my_ship.Move_vector.get_cords())
+
+                battle_output.UserCommands.append(
+                    UserCommand(Command="MOVE", Parameters=MoveCommandParameters(
+                        cur_my_ship.Id, cur_my_ship.Move_vector)))
+        battle_output.Message = f"To starting position..."
+        messages.clear()
+        return battle_output
